d23abf516d55c9ba9736791e11ad6278f34cae29
Polina Durdeva <Polina Durdeva>
2015-11-01 18:14:19 +0300

    add new page

--- EnglishCard/View/AddingNewWords.xaml.cs
+++ EnglishCard/View/AddingNewWords.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using EnglishCard.ViewModels;
+using EnglishCard.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -15,6 +17,38 @@ namespace EnglishCard.View
         public AddingNewWords()
         {
             InitializeComponent();
+            this.DataContext = App.ViewModel;
+        }
+
+        private void appBarOkButton_Click(object sender, EventArgs e)
+        {
+            // Confirm there is some text in the text box.
+            if (newOriginWordTextBox.Text.Length > 0)
+            {
+                // Create a new word.
+                Words newWord = new Words
+                {
+                    OriginWord = newOriginWordTextBox.Text,
+                    TranslateWord = newTranslateWordTextBox.Text,                   
+                };
+
+                // Add the item to the ViewModel.
+                App.ViewModel.AddWord(newWord);
+
+                // Return to the main page.
+                if (NavigationService.CanGoBack)
+                {
+                    NavigationService.GoBack();
+                }
+            }
+        }
+
+        private void appBarCancelButton_Click(object sender, EventArgs e)
+        {
+            if (NavigationService.CanGoBack)
+            {
+                NavigationService.GoBack();
+            }
         }
     }
 }
